657005db2513e87cc7340cf1c9b1eb14c2e95149
Johanna <hl105@wellesley.edu>
2024-02-11 14:19:02 -0500

    test

--- tests/tiktok/test_active.py
+++ tests/tiktok/test_active.py
@@ -3,13 +3,13 @@ import time
 
 def test_like_hashtag():
     # login with your active tiktok account
-    scenario_num_1 = 1 #change this number here for different scenarios!
-    username_1 = "Sec02Gr2Sc1Activ_JL" #replace JL with your initial
-    page1 = PageTiktok(scenario_num_1,username_1)
-    page1.fetch_tiktok()
+    scenario_num = 1 #change this number here for different scenarios!
+    username = "Sec02Gr2Sc1Activ_JL" #replace JL with your initial
+    page = PageTiktok(scenario_num,username)
+    page.fetch_tiktok()
     time.sleep(60)
-    page1.iterate_through_batches_like_by_hashtag()
-    #page1.chromebrowser.delete_all_cookies()
+    page.iterate_through_batches_like_by_hashtag()
+    #page.chromebrowser.delete_all_cookies()
     time.sleep(10)
 
 
